c003305a003a32ab6a1fc055ebdf27fe01c87f9f
I_D0nt_Know <mayank03ch@gmail.com>
2024-03-19 11:36:44 +0530

    added derver channel && member modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,7 @@ export default function RootLayout({
       )}>
       <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
-            forcedTheme="dark"  
+            defaultTheme="dark" 
             enableSystem={false}
             storageKey="ziscord-theme"
           >
--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -24,21 +24,21 @@ export const DeleteServerModal = () => {
 
   const [isLoading, setIsLoading] = useState(false);
 
-    const onClick = async () => {
-        try {
-            setIsLoading(true);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
 
-            await axios.delete(`/api/servers/${server?.id}`);
-            onClose();
-            router.refresh();
-            router.push("/");
+      await axios.delete(`/api/servers/${server?.id}`);
 
-        } catch (error) {
-            console.log(error);
-        }finally{
-            setIsLoading(false);
-        }
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -47,28 +47,28 @@ export const DeleteServerModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Delete Server
           </DialogTitle>
-          <DialogDescription className=" text-center text-zinc-500">
+          <DialogDescription className="text-center text-zinc-500">
             Are you sure you want to do this? <br />
-            <span className=" font-semibold text-indigo-500">{server?.name}</span> will be permanently deleted.
+            <span className="text-indigo-500 font-semibold">{server?.name}</span> will be permanently deleted.
           </DialogDescription>
         </DialogHeader>
-        <DialogFooter className=" bg-green-100 px-6 py-4">
-            <div className=" flex items-center justify-between w-full">
-                <Button
-                disabled={isLoading}
-                onClick={onClose}
-                variant= "ghost"
-                >
-                    Cancel
-                </Button>
-                <Button
-                disabled={isLoading}
-                variant="primary"
-                onClick={onClick}
-                >
-                    Confirm
-                </Button>
-            </div>
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isLoading}
+              onClick={onClose}
+              variant="ghost"
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={isLoading}
+              variant="primary"
+              onClick={onClick}
+            >
+              Confirm
+            </Button>
+          </div>
         </DialogFooter>
       </DialogContent>
     </Dialog>
